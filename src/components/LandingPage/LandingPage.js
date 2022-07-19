06208f7915b4d27bbfed63455ef3dd8c3354f67c
Prashant's Laptop <prashantraipm@gmail.com>
2022-07-19 16:38:11 +0530

    first

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,17 +2,9 @@ import React, { useState, useEffect } from 'react'
 import "./LandingPage.css"
 function LandingPage() {
 
-    const [landingData, setLandingData] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('')
-    const[filterData,setFilterData]=useState([])
-
-    const handleFilter=(event)=>{
-        const searchWord=event.target.value;
-        const newFilter=landingData.filter((value)=>{
-            return value.title.toLowerCase().includes(searchWord) ;
-        })
-        setFilterData(newFilter);
-    }
+    const [landingData, setLandingData] = useState([])
+    const [search, setSearch] = useState('')
+    const [output, setOutput] = useState([])
 
     const apiUrl = "https://s3-ap-southeast-1.amazonaws.com/he-public-data/gamesarena274f2bf.json"
     useEffect(() => {
@@ -21,32 +13,23 @@ function LandingPage() {
             .then(data => setLandingData(data))
 
     }, [])
-    // useEffect(()=>{
-    //     setOutput([])
+    // useEffect(() => {
+    //     setOutput(
+    //         landingData.output(value => {
+    //             return value.title.toLowerCase().include(search.toLowerCase())
+    //         }))
+    
+    // }, [search,landingData])
 
-    //     landingData.filter(val=>{
-    //         if(val.title.toLowerCase().includes(input.toLowerCase())){
-    //             setOutput(output=>[...output,val])
-
-    //     }
-    //     })
-    //   },[input])
     return (
         <>
             <div className='searchBar'>
                 <img src="https://i.pinimg.com/564x/72/3d/0a/723d0af616b1fe7d5c7e56a3532be3cd.jpg" alt='img' />
-                <input onInput={(event) => { setSearchTerm(event.target.value); }} type='text' placeholder='Search for games....' />
+                <input type='text' placeholder='Search for games....'  onChange={(e) => setSearch(e.target.value)} />
             </div>
             <div>
                 <div className='cont_cat'>
-                    {landingData.filter((value) => {
-                        if (searchTerm == "") {
-                            return value;
-                        }
-                        else if (value.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-                            return value;
-                        }
-                    }).map((value, key) => (
+                    {landingData.map((value, key) => (
                         <div className='cont'>
                             <div className='cont_data'>
                                 <p className='p_title'> {value.title}</p>
